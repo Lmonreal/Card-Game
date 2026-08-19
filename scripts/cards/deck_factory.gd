e98cffdba98671602e2d42f9bd9a6a98f95e6a26
Lorenzo Monreal <lmonreal2007@gmail.com>
2026-08-19 11:21:51 -0300

    Fix: Ace=14, chip value mapping, rank range 2-14

--- scripts/cards/deck_factory.gd
+++ scripts/cards/deck_factory.gd
@@ -2,10 +2,17 @@ extends Node
 class_name DeckFactory
 
 const int_rank_dict = {
-	1: "ace",
 	11: "jack", 
 	12: "queen",
 	13: "king", 
+	14: "ace",
+}
+
+const chip_correction_dict = {
+	11: 10,
+	12: 10,
+	13: 10,
+	14: 11,
 }
 
 const suit_string_dict = {
@@ -18,7 +25,7 @@ const suit_string_dict = {
 static func build_deck():
 	var deck : Array[Card]
 	for suit in range(0,4):
-		for num in range(1,14):
+		for num in range(2,15):
 			var card : Card = build_card(num, suit)
 			deck.append(card)
 	return deck
@@ -26,7 +33,7 @@ static func build_deck():
 static func build_card(card_value : int, suit : int) -> Card:
 	var card : Card = Card.new()
 	card.rank = card_value
-	card.chips = card_value
+	card.chips = chip_correction_dict.get(card_value, card_value)
 	card.suit = suit
 	card.name = int_rank_dict.get(card_value, str(card_value)) + " of " + str(suit_string_dict.get(suit))
 	var path = "res://assets/Cards/%s/%s.png" % [suit_string_dict.get(suit), int_rank_dict.get(card_value, str(card_value)) + "_of_" + str(suit_string_dict.get(suit))]
